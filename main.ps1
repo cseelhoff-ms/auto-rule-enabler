5d6031502b679558b7688188aa528e2ddc67130c
Caleb Seelhoff <cseelhoff@microsoft.com>
2023-10-09 17:22:59 -0500

    working KQL export

--- main.ps1
+++ main.ps1
@@ -21,26 +21,33 @@ Connect-AzAccount -ErrorAction Stop | Out-Null
 # Display all subscriptions for the logged in user
 Get-AzSubscription | Select-Object Name, Id | Format-Table
 
-# Select the subscription to use
-$subscriptionId = Read-Host -Prompt 'Enter the subscription ID (leave blank to select first subscription id)'
-if ($subscriptionId -eq '') {
+# if there is only 1 subscription, select it, otherwise prompt the user to select a subscription
+if ((Get-AzSubscription).Count -eq 1) {
     $subscriptionId = Get-AzSubscription | Select-Object -First 1 -ExpandProperty Id
+} else {
+    $subscriptionId = Read-Host -Prompt 'Enter the subscription ID:'
+    Set-AzContext -SubscriptionId $subscriptionId
 }
-Set-AzContext -SubscriptionId $subscriptionId
 
-# List all sentinel resources
-$sentinelWorkspaces = Get-AzResource -ResourceType Microsoft.OperationalInsights/workspaces | Select-Object Name, ResourceGroupName, Location
-$sentinelWorkspaces | Format-Table
+#get az resource for all solutions with the name starting with SecurityInsights(
+$securityInsightsSolutions = Get-AzResource -ResourceType Microsoft.OperationsManagement/solutions | Where-Object { $_.Name -like 'SecurityInsights(*' } | Select-Object -ExpandProperty Name | ForEach-Object { $_.Substring(17, $_.length -18) }
 
-# Select the sentinel workspace to use
-$workspaceName = Read-Host -Prompt 'Enter the workspace name (leave blank to select first workspace name)'
-if ($workspaceName -eq '') {
+# get all workspaces that are installed with sentinel
+$workspaces = Get-AzResource -ResourceType Microsoft.OperationalInsights/workspaces
+$sentinelWorkspaces = $workspaces | Where-Object { $_.Name -in $securityInsightsSolutions }
+$sentinelWorkspaces | Select-Object Name, ResourceGroupName, Location | Format-Table
+
+# if there is only 1 workspace, select it, otherwise prompt the user to select a workspace
+if ($sentinelWorkspaces.Count -eq 1) {
     $workspaceName = $sentinelWorkspaces | Select-Object -First 1 -ExpandProperty Name
+} else {
+    $workspaceName = Read-Host -Prompt 'Enter the workspace name:'
 }
+
 $resourceGroupName = $sentinelWorkspaces | Where-Object Name -eq $workspaceName | Select-Object -ExpandProperty ResourceGroupName
 
 # Set API version
-$apiVersion = '2023-06-01-preview'
+$apiVersion = '2023-07-01-preview'
 
 # Invoke API request to get templates
 $templates = Invoke-AzRestMethod -Method GET -path "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.OperationalInsights/workspaces/$workspaceName/providers/Microsoft.SecurityInsights/contentTemplates?api-version=$apiVersion"
@@ -48,35 +55,39 @@ $templates = Invoke-AzRestMethod -Method GET -path "/subscriptions/$subscription
 # Filter templates to get rule templates
 $ruleTemplates = $templates.Content | ConvertFrom-Json | Select-Object -ExpandProperty 'value' | Where-Object { $_.properties.contentKind -eq 'AnalyticsRule' }
 
+# Get existing rules
+$alertRulesJson =  Invoke-AzRestMethod -Method GET -path  "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.OperationalInsights/workspaces/$workspaceName/providers/Microsoft.SecurityInsights/alertRules?api-version=$apiVersion"
+$alertRules = $alertRulesJson.Content | ConvertFrom-Json | Select-Object -ExpandProperty 'value'
+
 # Output number of analytic rules found
 Write-Host "$($ruleTemplates.count) Analytic Rules found"
 
 # Loop through rule templates and create rules
 foreach ($ruleTemplate in $ruleTemplates) {
-    $ruleId = $ruleTemplate.properties.contentId
-    #$rule = $ruleTemplate.properties.mainTemplate.resources | Where-Object type -eq 'Microsoft.SecurityInsights/AlertRuleTemplates' | Select-Object -First 1
-    $rule = $ruleTemplate.properties.mainTemplate.resources | Where-Object type -Like '*AlertRule*' | Select-Object -First 1
-    $ruleName = $rule.properties.displayName
-    write-host "Processing rule $ruleName"
+    $templateRuleDisplayName = $ruleTemplate.properties.displayName
+    Write-Host "Processing rule: $templateRuleDisplayName"
     # Verify if the rule already exists
-    # FIX: Duplicates can still exist. Better to search on ruleTemplateId somehow
-    $apiPath = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.OperationalInsights/workspaces/$workspaceName/providers/Microsoft.SecurityInsights/alertRules/$($ruleId)?api-version=$apiVersion"
-    $result = Invoke-AzRestMethod -Method GET -path $apiPath
-    if ($result.StatusCode -ne 404) {
-        Write-Host "Rule $ruleName already exists" -ForegroundColor Yellow
+    $ruleTemplateContentId = $ruleTemplate.properties.contentId
+    $matchingRules =  $alertRules | Where-Object { $_.properties.alertRuleTemplateName -eq $ruleTemplateContentId }
+    if (($matchingRules | Measure-Object | Select-Object -ExpandProperty Count) -gt 0) {
+        Write-Host "Rule already exists`n" -ForegroundColor Yellow
         continue
-    }
-
+    }    
+    $ruleTemplateDetails = Invoke-AzRestMethod -Method GET -path ($ruleTemplate.id + '?api-version=' + $apiVersion)
+    $newRule = $ruleTemplateDetails.Content | ConvertFrom-Json | Select-Object -ExpandProperty properties | Select-Object -ExpandProperty mainTemplate | Select-Object -ExpandProperty resources | Select-Object -First 1
     # Create and enable rule
-    Write-Host "Creating and Enabling rule $ruleName... " -NoNewline
-    $rule.properties | Add-Member -NotePropertyName alertRuleTemplateName -NotePropertyValue $ruleId
-    $rule.properties | Add-Member -NotePropertyName templateVersion -NotePropertyValue $ruleTemplate.version
-    $rule.properties.enabled = $true
-    $payload = $rule | ConvertTo-Json -Depth 99
+    Write-Host "Creating and Enabling rule $templateRuleDisplayName... " -NoNewline
+    $newRule.properties | Add-Member -NotePropertyName alertRuleTemplateName -NotePropertyValue $ruleTemplateContentId
+    $newRule.properties | Add-Member -NotePropertyName templateVersion -NotePropertyValue $ruleTemplate.version
+    #$newRule.properties | Add-Member -NotePropertyName enabled -NotePropertyValue $true
+    #$newRule.properties.enabled = $true
+    $apiPath = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.OperationalInsights/workspaces/$workspaceName/providers/Microsoft.SecurityInsights/alertRules/$($ruleTemplateContentId)?api-version=$apiVersion"
+    $payload = $newRule | ConvertTo-Json -Depth 99
+    #$payload | Out-File "payload-$($ruleTemplateContentId).json"
     $result = Invoke-AzRestMethod -Method PUT -path $apiPath -Payload $payload
     if ($result.StatusCode -in 200, 201) {
-        Write-Host "Done" -ForegroundColor Green
+        Write-Host "Done`n" -ForegroundColor Green
     } else {
-        Write-Host $result.Content -ForegroundColor Red
+        Write-Host ($result.Content + "`n") -ForegroundColor Red
     }
 }
--- sigmaConverter.py
+++ sigmaConverter.py
@@ -4,9 +4,15 @@ from pathlib import Path
 from collections import defaultdict
 import copy
 from sigma.collection import SigmaCollection
+#from sigma.collection import SigmaDetection
+from typing import Dict, Any
+#from sigma.parser.modifiers import SigmaRuleLocation
+#from sigma.parser.modifiers import SigmaModifiers
+#from sigma.parser.condition import ConditionParser
+from sigma.parser.exceptions import SigmaParseError
 
-rules_root = 'C:\\Users\\cseelhoff\\source\\repos\\auto-rule-enabler\\sigma\\sigma-master\\rules'
-sigma_dict = defaultdict(dict)
+rules_root = 'C:\\Users\\cseelhoff\\source\\repos\\auto-rule-enabler\\sigma-master\\rules'
+sigma_dict: Dict[str, Dict[str, Any]] = defaultdict(dict)
 for file in Path(rules_root).resolve().rglob("*.yml"):
     rel_path = Path(file).relative_to(rules_root)
     path_key = '.'.join(rel_path.parent.parts)
@@ -23,24 +29,28 @@ for categ, sources in sigma_dict.items():
     #print("\n", categ, end="")
     for file_name, file_path in sources.items():
         print(file_path)
-        #sigma, kql = sigma_to_la(file_path)
 
-        #file_path = 'C:\\Users\\cseelhoff\\source\\repos\\auto-rule-enabler\\sigma\\sigma-master\\rules\\windows\\builtin\\shell_core\\win_shell_core_susp_packages_installed.yml'
-        #try:
         with open(file_path, 'r', encoding="utf8") as input_file:
             sigma_txt = input_file.read()
 
-        rules = SigmaCollection.from_yaml(sigma_txt)
-        for rule in rules:
+
+
+        try:
+            sigmaCollection: SigmaCollection = SigmaCollection.from_yaml(sigma_txt)
+        except SigmaParseError as e:
+            print(f"Error parsing {file_path}: {str(e)}")
+            continue
+        for rule in sigmaCollection.rules:
             #print(rule.detection)
             sigmaDetection = rule.detection
-            for detect in sigmaDetection.detections:
+            for detection in sigmaDetection.detections:
                 #print(detect)
-                detection_values = sigmaDetection.detections[detect]
+                detection_values: sigma.rule.SigmaDetection = sigmaDetection.detections[detection]
                 #print(detection_values)
                 for detection_item in detection_values.detection_items:
                     #if 'field' in detection_item:
-                    if hasattr(detection_item, 'field'):
+                    if hasattr(detection_item, 'field') and detection_item.field is not None:
+                        field: str = detection_item.field
                         if detection_item.field == 'EventID':
                             #print(detection_item.value, end=" ")
                             for detection_item_value in detection_item.value:
